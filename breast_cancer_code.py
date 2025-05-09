e3e5e4ba36764608c800bbec7beb4eb851080e7f
Williams Odunayo <williamsodunayo70@gmail.com>
2025-05-09 09:20:59 +0100

    Refactor breast cancer prediction script: add model training and evaluation, update visualization comments

--- breast_cancer_code.py
+++ breast_cancer_code.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 
 # Load the dataset
@@ -34,7 +36,7 @@ sns.countplot(data["diagnosis"], label="Count")
 plt.title("Count of Malignant and Benign Tumors")
 plt.xticks(rotation=90)
 plt.legend()
-#plt.show()
+"""plt.show()"""
 plt.close()
 
 # Convert the diagnosis column to numerical values with one-hot encoding
@@ -45,10 +47,37 @@ print(data["diagnosis"].value_counts())
 
 
 print(data.corr())
+# Plot correlation matrix
 plt.figure(figsize=(30, 30))
-# Plot the correlation matrix
 plt.title("Correlation Matrix")
 sns.heatmap(data.corr(), fmt=".2f", cmap="coolwarm")
-plt.show()
+"""plt.show()"""
 plt.close()
 
+# Create Pairplot based on diagnosis
+sns.pairplot(data.iloc[:, 1:5], hue="diagnosis") 
+"""plt.show()"""
+plt.close()
+
+# target and features
+X = data.drop(columns=["diagnosis"])
+y = data["diagnosis"]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=30)
+
+Scaler = StandardScaler()
+X_train = Scaler.fit_transform(X_train)
+X_test = Scaler.fit_transform(X_test)
+
+# Train the logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
